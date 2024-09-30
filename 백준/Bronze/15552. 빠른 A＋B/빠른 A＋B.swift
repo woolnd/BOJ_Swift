dcfd405008beecec8a197ccea000f26f58237975
엄재웅(RIU) <98391474+woolnd@users.noreply.github.com>
2024-10-01 07:59:26 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 228 ms, Memory: 107604 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.swift
@@ -0,0 +1,64 @@
+import Foundation
+
+// 라이노님의 FileIO
+final class FileIO {
+    private var buffer:[UInt8]
+    private var index: Int
+    
+    init(fileHandle: FileHandle = FileHandle.standardInput) {
+        buffer = Array(fileHandle.readDataToEndOfFile())+[UInt8(0)] // 인덱스 범위 넘어가는 것 방지
+        index = 0
+    }
+    
+    @inline(__always) private func read() -> UInt8 {
+        defer { index += 1 }
+        
+        return buffer.withUnsafeBufferPointer { $0[index] }
+    }
+    
+    @inline(__always) func readInt() -> Int {
+        var sum = 0
+        var now = read()
+        var isPositive = true
+        
+        while now == 10
+                || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        if now == 45{ isPositive.toggle(); now = read() } // 음수 처리
+        while now >= 48, now <= 57 {
+            sum = sum * 10 + Int(now-48)
+            now = read()
+        }
+        
+        return sum * (isPositive ? 1:-1)
+    }
+    
+    @inline(__always) func readString() -> String {
+        var str = ""
+        var now = read()
+        
+        while now == 10
+                || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        
+        while now != 10
+                && now != 32 && now != 0 {
+            str += String(bytes: [now], encoding: .ascii)!
+            now = read()
+        }
+        
+        return str
+    }
+}
+
+let fIO = FileIO()
+
+let inputCase = fIO.readInt()
+var result = ""
+
+for _ in 0..<inputCase {
+    let a = fIO.readInt(), b = fIO.readInt()
+    result += "\(a+b) \n"
+}
+
+//출력시 Enter이후 Control + D
+//https://gist.github.com/JCSooHwanCho/30be4b669321e7a135b84a1e9b075f88 참고해야함
+print(result)
