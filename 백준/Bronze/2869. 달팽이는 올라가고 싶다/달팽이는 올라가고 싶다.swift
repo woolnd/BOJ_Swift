74884870a6cc740017f0b3b31e53a6d41d7ee0ad
엄재웅(RIU) <98391474+woolnd@users.noreply.github.com>
2024-11-10 01:23:21 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 8 ms, Memory: 79512 KB -BaekjoonHub

---
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+if let input = readLine()?.split(separator: " "), input.count == 3{
+    
+    let inputNumber = input.compactMap{ Int($0) }
+    
+    let A = inputNumber[0]
+    let B = inputNumber[1]
+    let V = inputNumber[2]
+    
+    let day = (V - A + (A - B - 1)) / (A - B) + 1
+
+    print(day)
+}
