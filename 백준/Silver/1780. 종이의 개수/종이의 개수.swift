bc742adea6202302268f0a719f34948293e7ff29
엄재웅(RIU) <98391474+woolnd@users.noreply.github.com>
2025-02-06 10:58:52 +0900

    [Silver II] Title: 종이의 개수, Time: 1116 ms, Memory: 117276 KB -BaekjoonHub

---
+++ 백준/Silver/1780. 종이의 개수/종이의 개수.swift
@@ -0,0 +1,61 @@
+//
+//  main.swift
+//  BOJ
+//
+//  Created by wodnd on 9/16/24.
+//
+import Foundation
+
+let inputCase = Int(readLine() ?? "0") ?? 0
+var papers: [[Int]] = Array(repeating: Array(repeating: 0, count: inputCase), count: inputCase)
+
+for i in 0..<inputCase{
+    if let input = readLine()?.split(separator: " "), input.count == inputCase {
+        for j in 0..<inputCase{
+            papers[i][j] = Int(input[j]) ?? 0
+        }
+    }
+}
+
+var result: [Int] = Array(repeating: 0, count: 3)
+
+func Solution(y: Int, x: Int, N: Int){
+    let color = papers[y][x]
+    
+    for i in y..<y + N{
+        for j in x..<x + N{
+            if color != papers[i][j]{
+                let M = N / 3
+                Solution(y: y, x: x, N: M)
+                
+                Solution(y: y, x: x + M, N: M)
+                Solution(y: y, x: x + M + M, N: M)
+                
+                Solution(y: y + M, x: x, N: M)
+                Solution(y: y + M + M, x: x, N: M)
+                
+                Solution(y: y + M, x: x + M, N: M)
+                Solution(y: y + M, x: x + M + M, N: M)
+                Solution(y: y + M + M, x: x + M, N: M)
+                
+                Solution(y: y + M + M, x: x + M + M, N: M)
+                
+                
+                return
+            }
+        }
+    }
+    
+    if color == -1 {
+        result[0] += 1
+    } else if color == 0{
+        result[1] += 1
+    } else{
+        result[2] += 1
+    }
+}
+
+Solution(y: 0, x: 0, N: inputCase)
+print(result[0])
+print(result[1])
+print(result[2])
