99d406dad8a337c3c1a568d1118a9507046bd4d8
엄재웅(RIU) <98391474+woolnd@users.noreply.github.com>
2025-03-06 19:56:35 +0900

    [Silver II] Title: DFS와 BFS, Time: 24 ms, Memory: 79916 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.swift
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.swift
@@ -10,47 +10,47 @@ if let inputCase = readLine()?.split(separator: " "), inputCase.count == 3{
     let N = Int(inputCase[0]) ?? 0
     let M = Int(inputCase[1]) ?? 0
     let V = Int(inputCase[2]) ?? 0
-    
+
     var visited_dfs = Array(repeating: false, count: N + 1)
     var visited_bfs = visited_dfs
-    
+
     var graph: [[Int]] = Array(repeating: [], count: N + 1)
-    
+
     for i in 1...M{
         if let input = readLine()?.split(separator: " "), input.count == 2{
             let node = input.compactMap{ Int($0) }
-            
+
             graph[node[0]].append(node[1])
             graph[node[1]].append(node[0])
         }
     }
-    
+
     DFS(v: V)
     print()
     BFS(v: V)
     print()
-    
+
     func DFS(v: Int) {
         visited_dfs[v] = true
         print(v, terminator: " ")
-        
+
         for i in graph[v].sorted() {
             if !visited_dfs[i]{
                 DFS(v: i)
             }
         }
-        
+
     }
-    
+
     func BFS(v: Int) {
         visited_bfs[v] = true
-        
+
         var queue: [Int] = [v]
-        
+
         while queue.count != 0 {
             let v = queue.removeFirst()
             print(v, terminator: " ")
-            
+
             for i in graph[v].sorted(){
                 if !visited_bfs[i]{
                     queue.append(i)
@@ -58,6 +58,6 @@ if let inputCase = readLine()?.split(separator: " "), inputCase.count == 3{
                 }
             }
         }
-        
+
     }
 }
