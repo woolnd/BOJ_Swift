0a3bda1d67bd5649e015166d570ec7ad92f9981b
엄재웅(RIU) <98391474+woolnd@users.noreply.github.com>
2025-03-21 22:47:42 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 8 ms, Memory: 79512 KB -BaekjoonHub

--- 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.swift
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.swift
@@ -6,31 +6,28 @@
 //
 import Foundation
 
-if let input = readLine()?.split(separator: " ") {
-    let inputNumberA = Int(input[0]) ?? 0
-    let inputNumberB = Int(input[1]) ?? 0
+if let input = readLine()?.split(separator: " "), input.count == 2{
+    let inputA = Int(input[0]) ?? 0
+    let inputB = Int(input[1]) ?? 0
     
-    let gcf = GCF(inputA: inputNumberA, inputB: inputNumberB)
-    let lcm = LCM(inputA: inputNumberA, inputB: inputNumberB, gcf: gcf)
+    let gcd = GCD(inputA, inputB)
+    print(gcd)
+    print(LSM(gcd, inputA, inputB))
     
-    print(gcf)
-    print(lcm)
-}
-
-/// 최대공약수 (GCF) 계산 - 유클리드 알고리즘
-func GCF(inputA: Int, inputB: Int) -> Int {
-    var A = inputA
-    var B = inputB
-    
-    while B != 0 {
-        let remainder = A % B
-        A = B
-        B = remainder
+    func GCD(_ inputA: Int, _ inputB: Int) -> Int{
+        var a = inputA
+        var b = inputB
+        
+        while b != 0 {
+            let remainder = a % b
+            a = b
+            b = remainder
+        }
+        
+        return a
     }
-    return A
-}
-
-/// 최소공배수 (LCM) 계산
-func LCM(inputA: Int, inputB: Int, gcf: Int) -> Int {
-    return (inputA * inputB) / gcf
+    
+    func LSM(_ gcd: Int, _ inputA: Int, _ inputB: Int) -> Int{
+        return (inputA * inputB) / gcd
+    }
 }
