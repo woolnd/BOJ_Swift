cfc707970444cc17de0d090a0d4c913c7436d2aa
엄재웅(RIU) <98391474+woolnd@users.noreply.github.com>
2025-03-22 22:22:36 +0900

    [Silver III] Title: 13일의 금요일, Time: 16 ms, Memory: 79508 KB -BaekjoonHub

--- 백준/Silver/16463. 13일의 금요일/13일의 금요일.swift
+++ 백준/Silver/16463. 13일의 금요일/13일의 금요일.swift
@@ -6,109 +6,31 @@
 //
 import Foundation
 
-let inputYear = Int(readLine() ?? "") ?? 0
-
-var arrayYear: [Int] = []
-
-for i in 2019...inputYear{
-    arrayYear.append(i)
-}
-
-var chkLeapYear: [Bool] = []
-
-for i in 0..<arrayYear.count {
-    if arrayYear[i] % 400 == 0 {
-        chkLeapYear.append(true)
-    } else if arrayYear[i] % 400 != 0 && arrayYear[i] % 100 == 0{
-        chkLeapYear.append(false)
-    } else if arrayYear[i] % 100 != 0 && arrayYear[i] % 4 == 0{
-        chkLeapYear.append(true)
-    } else {
-        chkLeapYear.append(false)
-    }
-}
-
-var startDate: [String] = Array(repeating: "", count: arrayYear.count)
-startDate[0] = "화"
-
-var week = ["월", "화", "수", "목", "금", "토", "일"]
-var date = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-
-
-for i in 0..<arrayYear.count - 1 {
-    for j in 0..<week.count {
-        if startDate[i] == week[j]{
-            if chkLeapYear[i] {
-                if j == 5{
-                    startDate[i+1] = week[0]
-                } else if j == 6{
-                    startDate[i+1] = week[1]
-                } else {
-                    startDate[i+1] = week[j + 2]
-                }
-            } else {
-                if j == 6{
-                    startDate[i+1] = week[0]
-                } else {
-                    startDate[i+1] = week[j+1]
-                }
-            }
-        }
-    }
-}
-
+let input = Int(readLine() ?? "") ?? 0
 var count = 0
+var dayOfWeek = 1 //(0 = 월 ... 6 = 일)
 
-for i in 0..<arrayYear.count {
-    var monthStartDate: [String] = Array(repeating: "", count: 12)
-    monthStartDate[0] = startDate[i]
-    
-    if chkLeapYear[i] {
-        date[1] = 29
-    } else {
-        date[1] = 28
-    }
-    
-    for i in 0..<11 {
-        for j in 0..<week.count {
-            if monthStartDate[i] == week[j]{
-                if date[i] == 31 {
-                    if j == 4{
-                        monthStartDate[i + 1] = week[0]
-                    } else if j == 5{
-                        monthStartDate[i + 1] = week[1]
-                    } else if j == 6 {
-                        monthStartDate[i + 1] = week[2]
-                    } else {
-                        monthStartDate[i + 1] = week[j + 3]
-                    }
-                } else if date[i] == 30 {
-                    if j == 5{
-                        monthStartDate[i + 1] = week[0]
-                    } else if j == 6 {
-                        monthStartDate[i + 1] = week[1]
-                    } else {
-                        monthStartDate[i + 1] = week[j + 2]
-                    }
-                } else if date[i] == 29 {
-                    if j == 6 {
-                        monthStartDate[i + 1] = week[0]
-                    } else {
-                        monthStartDate[i + 1] = week[j + 1]
-                    }
-                } else {
-                    monthStartDate[i + 1] = week[j]
-                }
-            }
-        }
-    }
-    
-    for day in monthStartDate {
-        if day == "일" {
+let daysInMonth = [31,28,31,30,31,30,31,31,30,31,30,31]
+
+func chkLeapYear(_ year: Int) -> Bool{
+    if year % 400 == 0 { return true }
+    else if year % 400 != 0 && year % 100 == 0 { return false }
+    else if year % 100 != 0 && year % 4 == 0 { return true }
+    else { return false }
+}
+
+for year in 2019...input{
+    for month in 0..<12 {
+        let days = (month == 1 && chkLeapYear(year)) ? 29 : daysInMonth[month]
+        
+        let day13 = (dayOfWeek + 12) % 7
+        
+        if day13 == 4{
             count += 1
         }
+        
+        dayOfWeek = (days + dayOfWeek) % 7
     }
-
 }
 
 print(count)
