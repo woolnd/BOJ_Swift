a3c8facb40029c3ba228ee10fdaad92dc4ba57ea
엄재웅(RIU) <98391474+woolnd@users.noreply.github.com>
2025-04-27 03:23:11 +0900

    [Bronze V] Title: A-B, Time: 8 ms, Memory: 79512 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  BOJ
+//
+//  Created by wodnd on 9/16/24.
+//
+import Foundation
+
+let input = readLine()!.split(separator: " ")
+let A = Int(input[0])!
+let B = Int(input[1])!
+
+print(A-B)
