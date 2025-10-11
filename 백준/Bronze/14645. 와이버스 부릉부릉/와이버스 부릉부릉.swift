0209b403c22a6b0aaf5555183eca7c8b482a54d0
EumJaeWoong <98391474+woolnd@users.noreply.github.com>
2025-10-11 18:28:01 +0900

    [Bronze V] Title: 와이버스 부릉부릉, Time: 8 ms, Memory: 79512 KB -BaekjoonHub

---
+++ 백준/Bronze/14645. 와이버스 부릉부릉/와이버스 부릉부릉.swift
@@ -0,0 +1,16 @@
+//
+//  main.swift
+//  BOJ
+//
+//  Created by wodnd on 9/16/24.
+//
+import Foundation
+
+let input = readLine()!.split(separator: " ").map{ Int($0)! }
+let N = input[0], K = input[1]
+
+for _ in 0..<N {
+    let input = readLine()!.split(separator: " ").map{ Int($0)! }
+}
+
+print("비와이")
