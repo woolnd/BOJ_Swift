e6bd6881a421e17ee25d1e6aa346b4b6b13de9ee
EumJaeWoong <98391474+woolnd@users.noreply.github.com>
2025-10-27 14:06:03 +0900

    [Bronze III] Title: 웰컴 키트, Time: 8 ms, Memory: 79516 KB -BaekjoonHub

---
+++ 백준/Bronze/30802. 웰컴 키트/웰컴 키트.swift
@@ -0,0 +1,32 @@
+//
+//  main.swift
+//  BOJ
+//
+//  Created by wodnd on 9/16/24.
+//
+import Foundation
+
+let N = Int(readLine()!)!
+let tSize = readLine()!.split(separator: " ").map{ Int($0)! }
+
+let inputTP = readLine()!.split(separator: " ").map{ Int($0)! }
+let T = inputTP[0], P = inputTP[1]
+
+var resultT = 0
+
+for t in tSize {
+    if t == 0{
+        continue
+    }
+    
+    if T > t {
+        resultT += 1
+    } else if t % T == 0 {
+        resultT += t / T
+    } else {
+        resultT += (t / T + 1)
+    }
+}
+
+print(resultT)
+print("\(N / P) \(N % P)")
