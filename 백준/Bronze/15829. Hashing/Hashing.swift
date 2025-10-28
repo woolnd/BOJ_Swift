31f9ec1990d29bb64eb5198abd1cbff95502c9b8
EumJaeWoong <98391474+woolnd@users.noreply.github.com>
2025-10-28 10:08:08 +0900

    [Bronze II] Title: Hashing, Time: 8 ms, Memory: 79512 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.swift
+++ 백준/Bronze/15829. Hashing/Hashing.swift
@@ -8,19 +8,16 @@ import Foundation
 
 let L = Int(readLine()!)!
 let a = readLine()!.map{ String($0) }
-let M = 1234567891
-let r = 31
+let M: Int64 = 1234567891
+let r: Int64 = 31
 
-var result = 0
+var result: Int64 = 0
+var power: Int64 = 1
 
 for i in 0..<L {
     let ascii = Int(Character(a[i]).asciiValue! - Character("a").asciiValue! + 1)
-    
-    result += ascii * Int(pow(Double(r), Double(i)))
+    result = (result + Int64(ascii) * power) % M
+    power = (power * r) % M
 }
 
-if result < M {
-    print(result)
-} else {
-    print(result / M)
-}
+print(result)
