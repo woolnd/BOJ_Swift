3fe7afcc6633fb7155b213fb4d49faf5b1576816
JaeWoong Eum <98391474+woolnd@users.noreply.github.com>
2026-01-25 02:17:02 +0900

    [Gold III] Title: 웜홀, Time: 60 ms, Memory: 79912 KB -BaekjoonHub

---
+++ 백준/Gold/1865. 웜홀/웜홀.swift
@@ -0,0 +1,51 @@
+//
+//  main.swift
+//  BOJ
+//
+//  Created by wodnd on 9/16/24.
+//
+import Foundation
+
+let TC = Int(readLine()!)!
+
+for _ in 0..<TC {
+    let NMW = readLine()!.split(separator: " ").map{ Int($0)! }
+    let N = NMW[0], M = NMW[1], W = NMW[2]
+    
+    var edges: [(u: Int, v: Int, w: Int)] = []
+    
+    for _ in 0..<M {
+        let roadSET = readLine()!.split(separator: " ").map{ Int($0)! }
+        let S = roadSET[0], E = roadSET[1], T = roadSET[2]
+        edges.append((S, E, T))
+        edges.append((E, S, T))
+    }
+    
+    for _ in 0..<W {
+        let holeSET = readLine()!.split(separator: " ").map{ Int($0)! }
+        let S = holeSET[0], E = holeSET[1], T = holeSET[2]
+        edges.append((S, E, -T))
+    }
+    
+    var dist = Array(repeating: 0, count: N + 1)
+    var negativeFlag = false
+    
+    for i in 1...N {
+        for (u, v, w) in edges {
+            if dist[v] > dist[u] + w {
+                dist[v] = dist[u] + w
+                
+                if i == N {
+                    negativeFlag = true
+                    break
+                }
+            }
+        }
+    }
+    
+    if negativeFlag {
+        print("YES")
+    } else {
+        print("NO")
+    }
+}
