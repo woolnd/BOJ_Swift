75bd72ac100386587fa647eb56a21a84a48d34ca
JaeWoong Eum <98391474+woolnd@users.noreply.github.com>
2026-04-20 19:38:54 +0900

    Time: 4 ms (33.72%), Space: 19.8 MB (68.38%) - LeetHub

--- LeetCode/Swift/Easy/0001-two-sum/0001-two-sum.swift
+++ LeetCode/Swift/Easy/0001-two-sum/0001-two-sum.swift
@@ -1,16 +1,17 @@
 class Solution {
     func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
-        var dict = [Int: Int]()
-    
-        for (index, num) in nums.enumerated() {
-            let complement = target - num
-        
-            if let complementIndex = dict[complement] {
-                return [complementIndex, index]
+        var dict = [Int:Int]()
+
+        for (i, num) in nums.enumerated() {
+            let remain = target - num
+
+            if let index = dict[remain] {
+                return [index, i]
             }
-        
-            dict[num] = index
+
+            dict[num] = i
         }
+
         return []
     }
 }
