58d1dca722d3def51138872d5957f728cb413725
JaeWoong Eum <98391474+woolnd@users.noreply.github.com>
2026-05-08 15:50:34 +0900

    Time: 5 ms (8.6%), Space: 21.3 MB (36.25%) - LeetHub

---
+++ LeetCode/Swift/Medium/0011-container-with-most-water/0011-container-with-most-water.swift
@@ -0,0 +1,23 @@
+class Solution {
+    func maxArea(_ height: [Int]) -> Int {
+        var left = 0
+        var right = height.count - 1
+        var maxArea = 0
+
+        while left < right {
+            let width = right - left
+            let minHeight = min(height[left], height[right])
+
+            let currentArea = width * minHeight
+            maxArea = max(maxArea, currentArea)
+
+            if height[left] < height[right] {
+                left += 1
+            } else {
+                right -= 1
+            }
+        }
+
+        return maxArea
+    }
+}
