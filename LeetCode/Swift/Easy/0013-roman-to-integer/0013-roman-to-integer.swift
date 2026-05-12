95ff42f45737638d8da6154ed79ebf6d9edb826d
JaeWoong Eum <98391474+woolnd@users.noreply.github.com>
2026-05-12 13:03:34 +0900

    Time: 6 ms (29.45%), Space: 19.8 MB (71.98%) - LeetHub

---
+++ LeetCode/Swift/Easy/0013-roman-to-integer/0013-roman-to-integer.swift
@@ -0,0 +1,20 @@
+class Solution {
+    func romanToInt(_ s: String) -> Int {
+        var temp = s
+        var result = 0
+        let mapping: [(symbol: String, value: Int)] = [
+            ("M", 1000), ("CM", 900), ("D", 500), ("CD", 400),
+            ("C", 100), ("XC", 90), ("L", 50), ("XL", 40),
+            ("X", 10), ("IX", 9), ("V", 5), ("IV", 4), ("I", 1)
+        ]
+
+        for item in mapping {
+            while temp.hasPrefix(item.symbol) {
+                result += item.value
+                temp.removeFirst(item.symbol.count)
+            }
+        }
+
+        return result
+    }
+}
